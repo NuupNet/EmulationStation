cb985a7c1cad01687c997a45295a106cd26221cd
DoroteoArango <32972487+DoroteoArango@users.noreply.github.com>
2017-11-11 21:54:10 -0600

    Add files via upload

---
+++ es-core/src/Window.h
@@ -0,0 +1,100 @@
+#pragma once
+
+#include "GuiComponent.h"
+#include <vector>
+#include "resources/Font.h"
+#include "InputManager.h"
+
+class FileData;
+class HelpComponent;
+class ImageComponent;
+
+class Window
+{
+public:
+	class ScreenSaver {
+	public:
+		virtual void startScreenSaver() = 0;
+		virtual void stopScreenSaver() = 0;
+		virtual void nextVideo() = 0;
+		virtual void renderScreenSaver() = 0;
+		virtual bool allowSleep() = 0;
+		virtual void update(int deltaTime) = 0;
+		virtual bool isScreenSaverActive() = 0;
+		virtual FileData* getCurrentGame() = 0;
+		virtual void launchGame() = 0;
+	};
+
+	class InfoPopup {
+	public:
+		virtual void render(const Eigen::Affine3f& parentTrans) = 0;
+		virtual void stop() = 0;
+		virtual ~InfoPopup() {};
+	};
+
+	Window();
+	~Window();
+
+	void pushGui(GuiComponent* gui);
+	void removeGui(GuiComponent* gui);
+	GuiComponent* peekGui();
+	inline int getGuiStackSize() { return mGuiStack.size(); }
+
+	void textInput(const char* text);
+	void input(InputConfig* config, Input input);
+	void update(int deltaTime);
+	void render();
+
+	bool init(unsigned int width = 0, unsigned int height = 0);
+	void deinit();
+
+	void normalizeNextUpdate();
+
+	inline bool isSleeping() const { return mSleeping; }
+	bool getAllowSleep();
+	void setAllowSleep(bool sleep);
+
+	void renderLoadingScreen();
+
+	void renderHelpPromptsEarly(); // used to render HelpPrompts before a fade
+	void setHelpPrompts(const std::vector<HelpPrompt>& prompts, const HelpStyle& style);
+
+	void setScreenSaver(ScreenSaver* screenSaver) { mScreenSaver = screenSaver; }
+	void setInfoPopup(InfoPopup* infoPopup) { delete mInfoPopup; mInfoPopup = infoPopup; }
+	inline void stopInfoPopup() { if (mInfoPopup) mInfoPopup->stop(); };
+
+	void startScreenSaver();
+	void cancelScreenSaver();
+	void renderScreenSaver();
+
+private:
+	void onSleep();
+	void onWake();
+
+	// Returns true if at least one component on the stack is processing
+	bool isProcessing();
+	
+	HelpComponent* mHelp;
+	ImageComponent* mBackgroundOverlay;
+	ScreenSaver*	mScreenSaver;
+	InfoPopup*		mInfoPopup;
+	bool			mRenderScreenSaver;
+
+	std::vector<GuiComponent*> mGuiStack;
+
+	std::vector< std::shared_ptr<Font> > mDefaultFonts;
+
+	int mFrameTimeElapsed;
+	int mFrameCountElapsed;
+	int mAverageDeltaTime;
+
+	std::unique_ptr<TextCache> mFrameDataText;
+
+	bool mNormalizeNextUpdate;
+
+	bool mAllowSleep;
+	bool mSleeping;
+	unsigned int mTimeSinceLastInput;
+
+	bool mRenderedHelpPrompts;
+};
